9f1aba9ada1f403452904888400cd329d68351a1
ludovico99 <ludovico.zarr@gmail.com>
2023-04-20 18:33:43 +0200

    timer

--- Makefile
+++ Makefile
@@ -1,5 +1,15 @@
 all:
 	gcc -fopenmp main.c
 
-adder_dcop_32:
+dot-product-compile-seriale:
+	gcc -fopenmp dot-product-ludo.c
+
+dot-product-compile-parallel:
+	gcc -fopenmp dot-product-ludo.c -DPARALLEL
+
+timer-compile:
+	gcc -fopenmp -std=ec99 -D_POSIX_SOURCE -D_GNU_SOURCE dot-product-luca.c
+
+adder-dcop-32:
 	./a.out Matrici/adder_dcop_32/adder_dcop_32.mtx 
+
--- dot-product-ludo.c
+++ dot-product-ludo.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-unsigned long * generate_vector(unsigned long n)
+unsigned long *generate_vector(unsigned long n)
 {
-    unsigned long * vector;
+    unsigned long *vector;
     unsigned long i;
 
     vector = (unsigned long *)malloc(sizeof(unsigned long) * n);
 
-    if(vector == NULL)
+    if (vector == NULL)
     {
         printf("Errore malloc()\n");
         exit(1);
     }
 
-    for(i= 0; i<n; i++)
+    for (i = 0; i < n; i++)
     {
         vector[i] = 1;
     }
@@ -35,59 +35,133 @@ void serial_dot_product(unsigned long *v1, unsigned long *v2, unsigned long n)
 
     v3 = 0;
 
-    for(i=0; i<n; i++)
+    for (i = 0; i < n; i++)
     {
         v3 += v1[i] * v2[i];
-        for(j=0;j<120;j++)
+        for (j = 0; j < 120; j++)
         {
             y = v1[i] * v2[i];
         }
     }
-
-
 
     printf("Risultato prodotto scalare seriale %ld\n", v3);
 
     return;
 }
 
-
-void parallel_dot_product_1(unsigned long *v1, unsigned long *v2, unsigned long n)
+/*La sintassi per realizzare un’operazione di riduzione in openMP `e la seguente:
+#pragma omp parallel reduction(operazione: nomevariabile)*/
+void parallel_dot_product_red(unsigned long *v1, unsigned long *v2, unsigned long n)
 {
     unsigned long i;
     unsigned long v3;
     unsigned long y;
     unsigned long j;
+    int nthreads;
+
+    v3 = 0;
+    y = 0;
+    j = 0;
+
+    nthreads = 10;
+#pragma omp parallel num_threads(nthreads) shared(n, v1, v2) firstprivate(y, j) reduction (+: v3) default(none)
+    {
+        printf("Sono il thread %d\n", omp_get_thread_num());
+#pragma omp for schedule(dynamic)
+        for (i = 0; i < n; i++)
+        {
+            v3 += v1[i] * v2[i];
+            for (j = 0; j < 120; j++)
+            {
+                y = v1[i] * v2[i];
+            }
+        }
+    }
+
+    printf("Risultato prodotto scalare parallelo con dynamic %ld\n", v3);
+
+    return;
+}
+
+/*Ogni thread esegue un chunk di iterazioni e quindi richiede un altro chunk fino a quando non ci sono pi`u chunk disponibili.*/
+void parallel_dot_product_dynamic(unsigned long *v1, unsigned long *v2, unsigned long n)
+{
+    unsigned long i;
+    unsigned long v3;
+    unsigned long y;
+    unsigned long j;
+    int nthreads;
 
     v3 = 0;
     y = 0;
     j = 0;
 
     unsigned long partial_sum = 0;
-
-    #pragma omp parallel for schedule(dynamic) num_threads(10) shared(n, v1, v2) firstprivate(partial_sum, y, j) default(none)   
-
-        for(i=0; i<n; i++)
+    nthreads = 10;
+#pragma omp parallel num_threads(nthreads) shared(n, v1, v2, v3) firstprivate(partial_sum, y, j) default(none)
+    {
+        printf("Sono il thread %d\n", omp_get_thread_num());
+#pragma omp for schedule(dynamic)
+        for (i = 0; i < n; i++)
         {
             partial_sum += v1[i] * v2[i];
-            for(j=0;j<120;j++)
+            for (j = 0; j < 120; j++)
             {
                 y = v1[i] * v2[i];
             }
         }
-    #pragma omp critical(v3)
-    {
-        v3 += partial_sum;
+#pragma omp critical
+        {
+            v3 += partial_sum;
+        }
     }
 
-    
-
-
-    printf("Risultato prodotto scalare parallelo %ld\n", v3);
+    printf("Risultato prodotto scalare parallelo con dynamic %ld\n", v3);
 
     return;
 }
 
+/* La differenza con il tipo di pianificazione dinamica `e nella dimensione dei chunk.
+La dimensione di un chunk `e proporzionale al numero di iterazioni non assegnate diviso per il numero dei thread.
+Pertanto la dimensione dei chunk diminuisce.*/
+void parallel_dot_product_guided(unsigned long *v1, unsigned long *v2, unsigned long n)
+{
+    unsigned long i;
+    unsigned long v3;
+    unsigned long y;
+    unsigned long j;
+    unsigned long nthreads;
+
+    v3 = 0;
+    y = 0;
+    j = 0;
+
+    unsigned long partial_sum = 0;
+    nthreads = 10;
+#pragma omp parallel num_threads(nthreads) shared(n, v1, v2, v3) firstprivate(partial_sum, y, j) default(none)
+    {
+        printf("Sono il thread %d\n", omp_get_thread_num());
+
+        #pragma omp for schedule(guided)
+        for (i = 0; i < n; i++)
+        {
+            partial_sum += v1[i] * v2[i];
+            for (j = 0; j < 120; j++)
+            {
+                y = v1[i] * v2[i];
+            }
+        }
+
+#pragma omp critical
+            {
+                v3 += partial_sum;
+            }
+    }
+
+    printf("Risultato prodotto scalare parallelo con guided %ld\n", v3);
+
+    return;
+}
 
 int main(int argc, char **argv)
 {
@@ -95,7 +169,7 @@ int main(int argc, char **argv)
     unsigned long *v1;
     unsigned long *v2;
 
-    if(argc!=2)
+    if (argc != 2)
         exit(1);
 
     n = atoi(argv[1]);
@@ -104,7 +178,11 @@ int main(int argc, char **argv)
 
     v2 = generate_vector(n);
 
-    //serial_dot_product(v1, v2, n);
-
-    parallel_dot_product_1(v1, v2, n);
+#ifndef PARALLEL
+    serial_dot_product(v1, v2, n);
+#else
+    parallel_dot_product_dynamic(v1, v2, n);
+    //parallel_dot_product_guided(v1, v2, n);
+    //parallel_dot_product_red(v1, v2, n);
+#endif
 }
