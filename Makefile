ce5207db350bac3477052a0f1f9631bfc962f4a3
ludovico99 <ludovico.zarr@gmail.com>
2023-04-20 16:08:49 +0200

    modifiche

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	gcc -fopenmp main.c
+
+adder_dcop_32:
+	./a.out Matrici/adder_dcop_32/adder_dcop_32.mtx 
---
+++ main.c
@@ -0,0 +1,80 @@
+#include "omp.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "mmio.c"
+
+int main(int argc, char *argv[])
+{
+    int ret_code;
+    MM_typecode matcode;
+    FILE *f;
+    int nthreads;
+    int M, N, nz;
+    int *I, *J;
+    double *val;
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s [matrix-market-filename]\n", argv[0]);
+        exit(1);
+    }
+    else
+    {
+        if ((f = fopen(argv[1], "r")) == NULL)
+            exit(1);
+    }
+
+    if (mm_read_banner(f, &matcode) != 0)
+    {
+        printf("Could not process Matrix Market banner.\n");
+        exit(1);
+    }
+
+    /*  This is how one can screen matrix types if their application */
+    /*  only supports a subset of the Matrix Market data types.      */
+    if (mm_is_sparse(matcode))
+    {
+        /* find out size of sparse matrix .... */
+
+        if ((ret_code = mm_read_mtx_crd_size(f, &M, &N, &nz)) != 0)
+            exit(1);
+
+        I = (int *)malloc(nz * sizeof(int));
+        J = (int *)malloc(nz * sizeof(int));
+        val = (double *)malloc(nz * sizeof(double));
+    nthreads = nz / 16;
+    #pragma omp parallel for schedule (static, 16) num_threads(nthreads) default(none)
+        for (int i = 0; i < nz; i++)
+        {
+            fscanf(f, "%d %d %lg\n", &I[i], &J[i], &val[i]);
+            I[i]--; /* adjust from 1-based to 0-based */
+            J[i]--;
+            printf("%d %d %lg\n", I[i], J[i], val[i]);
+        }
+    }
+    else
+    {
+        printf("Sorry, this application does not support ");
+        printf("Market Market type: [%s]\n", mm_typecode_to_str(matcode));
+        exit(1);
+    }
+
+    if (f != stdin)
+        fclose(f);
+
+    // /************************/
+    // /* now write out matrix */
+    // /************************/
+
+    // mm_write_banner(stdout, matcode);
+    // mm_write_mtx_crd_size(stdout, M, N, nz);
+    // for (i = 0; i < nz; i++)
+    //     fprintf(stdout, "%d %d %20.19g\n", I[i] + 1, J[i] + 1, val[i]);
+
+    return 0;
+}
