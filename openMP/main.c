135274c2cf1c6a433da9a22617c1cc1f6d16f8e0
ludovico99 <ludovico.zarr@gmail.com>
2023-05-06 12:35:27 -0400

    added variance

--- openMP/main.c
+++ openMP/main.c
@@ -6,12 +6,13 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <time.h>
+#include <math.h>
 
 #include "mmio.h"
 #include "header.h"
 
 
-#define SAMPLING_SIZE 10
+#define SAMPLING_SIZE 30
 
 void create_dense_matrix(int N, int K, double ***x)
 {
@@ -59,6 +60,22 @@ FILE *init_stream(const char *filename)
 
     return f;
 }
+
+double calculate_mean(double x, double mean, int n) {
+    mean += (x - mean) / n;
+    return mean;
+}
+
+double calculate_variance(double x, double mean, double variance, int n) {
+    if (n == 1) {
+        return 0.0;
+    }
+    double delta = x - mean;
+    mean = calculate_mean(x, mean, n);
+    variance += delta * (x - mean);
+    return variance / (n - 1);
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -328,59 +345,56 @@ int main(int argc, char *argv[])
         coo_to_CSR_parallel(M, N, nz, I, J, val, &as_A, &ja_A, &irp_A);
     #endif
     
+    double mean = 0.0;
+    double time = 0.0;
+    double variance = 0.0;
+    const char *filename; 
 
-    
 
     #ifdef BOTH
-        double mean;
-        double time;
-        const char *filename; 
-
+  
         FILE *f_samplings_serial;
         FILE *f_samplings_parallel;
         #ifdef ELLPACK
             filename = "samplings_parallel_ELLPACK.csv";
             f_samplings_parallel = fopen(filename, "w+");
-            fprintf(f_samplings_parallel, "K,num_threads,mean\n");
+            fprintf(f_samplings_parallel, "K,num_threads,mean,variance\n");
 
             filename = "samplings_serial_ELLPACK.csv";
             f_samplings_serial = fopen(filename, "w+");
-            fprintf(f_samplings_serial, "K,mean\n");
+            fprintf(f_samplings_serial, "K,mean,variance\n");
         #else
             filename = "samplings_parallel_CSR.csv";
             f_samplings_parallel = fopen(filename, "w+");
-            fprintf(f_samplings_parallel, "K,num_threads,mean\n");
+            fprintf(f_samplings_parallel, "K,num_threads,mean,variance\n");
 
             filename = "samplings_serial_CSR.csv";
             f_samplings_serial = fopen(filename, "w+");
-            fprintf(f_samplings_serial, "K,mean\n");
+            fprintf(f_samplings_serial, "K,mean,variance\n");
         #endif
 
     #else
 
-        double mean;
-        double time;
-        const char *filename; 
         FILE *f_samplings;
         #ifdef PARALLEL_SAMPLING
             #ifdef ELLPACK
                 filename = "samplings_parallel_ELLPACK.csv";
                 f_samplings = fopen(filename, "w+");
-                fprintf(f_samplings, "K,num_threads,mean\n");
+                fprintf(f_samplings, "K,num_threads,mean,variance\n");
             #else
                 filename = "samplings_parallel_CSR.csv";
                 f_samplings = fopen(filename, "w+");
-                fprintf(f_samplings, "K,num_threads,mean\n");
+                fprintf(f_samplings, "K,num_threads,mean,variance\n");
             #endif
         #else
             #ifdef ELLPACK
                 filename = "samplings_serial_ELLPACK.csv";
                 f_samplings = fopen(filename, "w+");
-                fprintf(f_samplings, "K,mean\n");
+                fprintf(f_samplings, "K,mean,variance\n");
             #else
                 filename = "samplings_serial_CSR.csv";
                 f_samplings = fopen(filename, "w+");
-                fprintf(f_samplings, "K,mean\n");
+                fprintf(f_samplings, "K,mean,variance\n");
             #endif
         #endif
     #endif
@@ -392,8 +406,8 @@ int main(int argc, char *argv[])
             #ifdef PARALLEL_SAMPLING
             for (int num_thread = 1; num_thread <= nthread; num_thread++){
             #endif
-                mean = 0;
-
+                mean = 0.0;
+                variance = 0.0;
                 for (int curr_samp = 0; curr_samp < SAMPLING_SIZE; curr_samp++)
                 {   
                     #ifdef ELLPACK
@@ -413,19 +427,18 @@ int main(int argc, char *argv[])
                         #endif
                     #endif
 
-                    mean += time;
+                    mean = calculate_mean(time, mean, curr_samp + 1);
+                    variance = calculate_variance(time, mean, variance, curr_samp + 1);
                 }
-
-                mean = mean / SAMPLING_SIZE;
                 
             #ifdef PARALLEL_SAMPLING
                 printf("MEAN for K %d, num_thread %d is %lf\n", K[k], num_thread, mean);
-                fprintf(f_samplings, "%d,%d,%lf\n", K[k], num_thread, mean);
+                fprintf(f_samplings, "%d,%d,%lf,%.20lf\n", K[k], num_thread, mean, variance);
                 fflush(f_samplings);
                 }
             #else 
                 printf("MEAN for K %d is %lf\n", K[k], mean);
-                fprintf(f_samplings, "%d,%lf\n", K[k], mean);
+                fprintf(f_samplings, "%d,%lf,%.20lf\n", K[k], mean, variance);
                 fflush(f_samplings);
             #endif
 
@@ -442,8 +455,8 @@ int main(int argc, char *argv[])
         {
             create_dense_matrix(N, K[k], &X);
             for (int num_thread = 1; num_thread <= nthread; num_thread++){
-                mean = 0;
-
+                mean = 0.0;
+                variance = 0.0;
                 for (int curr_samp = 0; curr_samp < SAMPLING_SIZE; curr_samp++)
                 {   
                     #ifdef ELLPACK
@@ -453,12 +466,12 @@ int main(int argc, char *argv[])
                         y_parallel = parallel_product_CSR(M, N, K[k], nz, as_A, ja_A, irp_A, X, &time, num_thread); 
                     #endif
 
-                    mean += time;
+                    mean = calculate_mean(time, mean, curr_samp + 1);
+                    variance = calculate_variance(time, mean, variance, curr_samp + 1);
                 }
 
-                mean = mean / SAMPLING_SIZE;
                 printf("MEAN for K %d, num_thread %d is %lf\n", K[k], num_thread, mean);
-                fprintf(f_samplings_parallel, "%d,%d,%lf\n", K[k], num_thread, mean);
+                fprintf(f_samplings_parallel, "%d,%d,%lf,%.20lf\n", K[k], num_thread, mean, variance);
                 fflush(f_samplings_parallel);
             }
 
@@ -474,8 +487,8 @@ int main(int argc, char *argv[])
         {
             create_dense_matrix(N, K[k], &X);
             
-            mean = 0;
-
+            mean = 0.0;
+            variance = 0.0;
             for (int curr_samp = 0; curr_samp < SAMPLING_SIZE; curr_samp++)
             {   
                     #ifdef ELLPACK
@@ -485,12 +498,12 @@ int main(int argc, char *argv[])
                         y_serial = serial_product_CSR(M, N, K[k], nz, as_A, ja_A, irp_A, X, &time);
                     #endif
 
-                    mean += time;
+                    mean = calculate_mean(time, mean, curr_samp + 1);
+                    variance = calculate_variance(time, mean, variance, curr_samp + 1);
                 }
 
-                mean = mean / SAMPLING_SIZE;
                 printf("MEAN for K %d is %lf\n", K[k], mean);
-                fprintf(f_samplings_serial, "%d,%lf\n", K[k], mean);
+                fprintf(f_samplings_serial, "%d,%lf,%.20lf\n", K[k], mean, variance);
                 fflush(f_samplings_serial);
             
 
--- openMP/plot-performace.py
+++ openMP/plot-performace.py
@@ -2,17 +2,39 @@ import pandas
 import matplotlib.pyplot as plt
 import numpy as np
 	
+import scipy.stats as stats
+
+# def confidence_band_t(mean, std_dev, n):
+#     t_score = stats.t.ppf(0.975, n-1)
+#      z_score = stats.norm.ppf(0.975)
+#     lower = []
+#     upper = []
+#     for i in range(len(mean_seq)):
+#         lower.append(mean_seq[i] - z_score * np.sqrt(var_seq[i] / n_seq[i]))
+#         upper.append(mean_seq[i] + z_score * np.sqrt(var_seq[i] / n_seq[i]))
+#     return lower, upper
+
+
 def print_all_results(samplings_parallel,sampling_serial, k):
 
+	n = 30
 	plt.style.use('seaborn-v0_8')
-	plt.grid(color='grey', linestyle='-', linewidth=0.8, alpha=0, axis='x')
+	plt.grid(color='grey', linestyle='-', linewidth=0.8, alpha=0.2, axis='x')
 	plt.grid(color='grey', linestyle='-', linewidth=0.8, alpha=0.2, axis='y')
 	
 	fig, ax = plt.subplots()
 	fig.set_size_inches(16, 9)
 
-	ax.plot(samplings_parallel[0], samplings_parallel[1], label="Plot di mean al variare del numero di thread e K fissato", linewidth=2.0, color='indigo')
-	ax.scatter(1, sampling_serial, label="Mean value for serial product with K fixed", color='red')
+	ax.plot(samplings_parallel[0], samplings_parallel[1],marker='o', markersize=2, label="Mean value with K fixed and variable num_thread", linewidth=2.0, color='indigo')
+	# lower, upper = confidence_band_t(samplings_parallel[1], samplings_parallel[2], len(samplings_parallel[1]))
+	mean = samplings_parallel[1]
+	variance = samplings_parallel[2]
+	std = np.sqrt(variance)
+	ci = stats.t.interval(0.95, n-1, loc = mean, scale=std/np.sqrt(n))
+	plt.fill_between(samplings_parallel[0], ci[0], ci[1],
+                    color='darkviolet', alpha=0.1,
+                    label="Confidence band of 95")
+	ax.scatter(1, sampling_serial[0], label="Mean value for serial product with K fixed", color='red')
 	ax.legend(loc='upper right', shadow=True, fontsize=10)
 
 	plt.title("Plot di (num_threads, media dei tempi) fissato K ={} ".format(k), fontsize=20, fontname='DejaVu Sans', weight='bold', style='italic')
@@ -26,16 +48,17 @@ def print_all_results(samplings_parallel,sampling_serial, k):
 	
 	
 
-samplings_3_parallel = [[],[]]
-samplings_4_parallel  = [[],[]]
-samplings_8_parallel  = [[],[]]
-samplings_12_parallel  = [[],[]]
-samplings_16_parallel  = [[],[]]
-samplings_32_parallel  = [[],[]]
-samplings_64_parallel  = [[],[]]
+samplings_3_parallel = [[],[],[]]
+samplings_4_parallel  = [[],[],[]]
+samplings_8_parallel  = [[],[],[]]
+samplings_12_parallel  = [[],[],[]]
+samplings_16_parallel  = [[],[],[]]
+samplings_32_parallel  = [[],[],[]]
+samplings_64_parallel  = [[],[],[]]
 
 
 # Leggo le misure delle prestazioni per il parallelo
+# df_parallel = pandas.read_csv("samplings_parallel_ELLPACK.csv", dtype={'K': 'int64','num_thread': 'int64','mean': 'float64'})
 df_parallel = pandas.read_csv("samplings_parallel_CSR.csv", dtype={'K': 'int64','num_thread': 'int64','mean': 'float64'})
 
 for row in df_parallel.itertuples(index= False):
@@ -43,24 +66,31 @@ for row in df_parallel.itertuples(index= False):
 		if(row[0] == 3):
 			samplings_3_parallel[0].append(row[1])
 			samplings_3_parallel[1].append(row[2])
+			samplings_3_parallel[2].append(row[3])
 		elif(row[0] == 4):
 			samplings_4_parallel[0].append(row[1])
 			samplings_4_parallel[1].append(row[2])
+			samplings_4_parallel[2].append(row[3])
 		elif(row[0] == 8):
 			samplings_8_parallel[0].append(row[1])
 			samplings_8_parallel[1].append(row[2])
+			samplings_8_parallel[2].append(row[3])
 		elif(row[0] == 12):
 			samplings_12_parallel[0].append(row[1])
 			samplings_12_parallel[1].append(row[2])
+			samplings_12_parallel[2].append(row[3])
 		elif(row[0] == 16):
 			samplings_16_parallel[0].append(row[1])
 			samplings_16_parallel[1].append(row[2])
+			samplings_16_parallel[2].append(row[3])
 		elif(row[0] == 32):
 			samplings_32_parallel[0].append(row[1])
 			samplings_32_parallel[1].append(row[2])
+			samplings_32_parallel[2].append(row[3])
 		elif(row[0] == 64):
 			samplings_64_parallel[0].append(row[1])
 			samplings_64_parallel[1].append(row[2])
+			samplings_64_parallel[2].append(row[3])
 		else:
 			print("Valore di K non ammissibile.")
 	except:
@@ -68,24 +98,40 @@ for row in df_parallel.itertuples(index= False):
 		exit(1)
 
 # Leggo le misure delle prestazioni per il seriale
+#df_serial = pandas.read_csv("samplings_serial_ELLPACK.csv", dtype={'K': 'int64','mean': 'float64', 'variance': 'float64'})
 df_serial = pandas.read_csv("samplings_serial_CSR.csv", dtype={'K': 'int64','mean': 'float64'})
+
+samplings_3_serial = []
+samplings_4_serial  = []
+samplings_8_serial  = []
+samplings_12_serial  = []
+samplings_16_serial  = []
+samplings_32_serial  = []
+samplings_64_serial  = []
 
 for row in df_serial.itertuples(index= False):
 	try:	
 		if(row[0] == 3):
-			samplings_3_serial = row[1]
+			samplings_3_serial.append(row[1])
+			samplings_3_serial.append(row[2])
 		elif(row[0] == 4):
-			samplings_4_serial = row[1]
+			samplings_4_serial.append(row[1])
+			samplings_4_serial.append(row[2])
 		elif(row[0] == 8):
-			samplings_8_serial= row[1]
+			samplings_8_serial.append(row[1])
+			samplings_8_serial.append(row[2])
 		elif(row[0] == 12):
-			samplings_12_serial= row[1]
+			samplings_12_serial.append(row[1])
+			samplings_12_serial.append(row[2])
 		elif(row[0] == 16):
-			samplings_16_serial= row[1]
+			samplings_16_serial.append(row[1])
+			samplings_16_serial.append(row[2])
 		elif(row[0] == 32):
-			samplings_32_serial= row[1]
+			samplings_32_serial.append(row[1])
+			samplings_32_serial.append(row[2])
 		elif(row[0] == 64):
-			samplings_64_serial= row[1]
+			samplings_64_serial.append(row[1])
+			samplings_64_serial.append(row[2])
 		else:
 			print("Valore di K non ammissibile.")
 	except:
